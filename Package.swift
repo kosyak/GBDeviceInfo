93649f13310982054675a437108e67a0c2fd411b
Vadim <573626+kosyak@users.noreply.github.com>
2021-04-18 02:05:45 +0300

    Removed OSX stuff

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "GBDeviceInfo",
 	platforms: [
 		.iOS(.v8),
-		.macOS(.v10_10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -26,7 +25,7 @@ let package = Package(
             name: "GBDeviceInfo",
             dependencies: [],
 			path: "GBDeviceInfo",
-			sources: ["GBDeviceInfo_Common.m", "GBDeviceInfo_iOS.m","GBDeviceInfo_OSX.m"],
+			sources: ["GBDeviceInfo_Common.m", "GBDeviceInfo_iOS.m"],
 			publicHeadersPath: ""
 		),
 		
