6cb4b93fbc8f04cd552bdac49326b5ae92082133
Vadim <573626+kosyak@users.noreply.github.com>
2024-08-24 23:29:11 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "DeviceInfo",
+    name: "GBDeviceInfo",
 	platforms: [
 		.iOS(.v9),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DeviceInfo",
-            targets: ["DeviceInfo"]),
+            name: "GBDeviceInfo",
+            targets: ["GBDeviceInfo"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
-            name: "DeviceInfo",
+            name: "GBDeviceInfo",
             dependencies: [],
 			path: "GBDeviceInfo",
 			sources: ["GBDeviceInfo_Common.m", "GBDeviceInfo_iOS.m"],
